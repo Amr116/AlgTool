bbef132b4bfea18dc652489a521555905ef05739
runefranch <rune.franch@gmail.com>
2016-05-18 00:25:39 +0200

    Added content on BB
    uge 2 new type buildblocks
    used in BuildBlockManager

---
+++ assignments/uge2/Floor.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+class BuildBlock:
+    def show_in_menu():
+        Button = '<div style="float: left">|_ </div>' \
+                 '<div class="contain">Expr</div>' \
+                 '<div style="float: left"> _|</div>'
+        return Button
+
+    def show_in_screen():
+        Screen = '<div class = pluscontainer style="display: inline-block; min-width: 20px; max-height: 50px;">' \
+                 '<div style="float: left">|_ </div>' \
+                 '<div class="contain" id="floor" ondrop="drop(event)" ondragover="allowDrop(event)"></div>' \
+                 '<div style="float: left"> _|</div>' \
+                 '</div>'
+        return Screen
---
+++ assignments/uge2/ValueConst.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+class BuildBlock:
+    def show_in_menu():
+        Button = '<div class="contain">Constant</div>'
+        return Button
+
+    def show_in_screen():
+        Screen = '<div class="variable"><input type="number" class="constant"/>'
+        return Screen
