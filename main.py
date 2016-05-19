6e8d0395aef651d8f19ad9ec0b9722d4b4a3255a
Amr Elsayed <amr11682@hotmail.com>
2016-05-19 15:33:59 +0200

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from server.HttpServer import HttpServer
 
 if __name__ == '__main__':
-    server = HttpServer('localhost', 8000)
+    server = HttpServer('localhost', 8080)
     server.start_server()
