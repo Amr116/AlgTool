9de13919d39576e88a4fbddd3550d10c16fc5544
Amr Elsayed <amr11682@hotmail.com>
2016-06-04 19:56:45 +0200

    add list of exsits folder to home page

--- protocol/HttpGet.py
+++ protocol/HttpGet.py
@@ -6,52 +6,87 @@ from protocol.HttpRequest import HttpRequest
 from BuildBlockManager import BbManager
 
 class HttpGet(HttpRequest):
+
     def __init__(self, match, csock):
 
         filename = match.split("/")[-1]
+        if filename.endswith(".html") or filename.endswith(".js") or filename.endswith(".css"):
+            filename = filename
+        else:
+            filename = ""
+
         filepath = match.replace(filename, "")
 
-        if filename is None or filepath is None or filepath is "/":  # directory index requested
-            filename = "index.html"
-            filepath = "/"
-            if Filesystem.file_exists(filepath, filename):  # we look for index.html
-                #self.serve_file(filepath, filename, csock)
-                (BuildBlockButton, BuildBlockScreen) = BbManager.start("assignments/week2/")
 
-                with open(Options.root_dir + "/template/header.html", 'r') as fhandle:
-                    Header = '<!DOCTYPE html>\n<html lang="en">\n' + fhandle.read()
 
-                with open(Options.root_dir + "/template/topbar.html", 'r') as fhandle:
-                    topbar = '<body>\n<div class="container-fluid">\n' + fhandle.read()
+        if (((filename == "index.html") or (filename == "")) and (filepath == "") or (filepath == "/")):
+             filename = "index.html"
+             filepath = "/"
+             f = self.serve_index()
+             csock.sendall(("""HTTP/1.1 200 OK
+                Content-Type: text/html
+                Content-Length: """ + str(len(f)) + """
+                \n\n""" + f + """\r\n""").encode())
+             """
+             if Filesystem.file_exists(filepath, filename):
+                # here i will server home page with list dir for all the
+                createIndex() 
+                self.serve_file(filepath, filename, csock)
+            """
 
-                ButtonMenu = '<div class="row down-container">\n<div class="col-lg-3">\n<div class="row cluster">' + \
-                            BuildBlockButton + '</div>\n</div>\n'
-
-                with open(Options.root_dir + "/template/algoscreen.html", 'r') as fhandle:
-                    AlgorithmScreen = fhandle.read()
-
-                with open(Options.root_dir + "/template/logcontainer.html", 'r') as fhandle:
-                    LogContainer = fhandle.read() + '</div>\n'
-
-                ShowBBInScreen = 'function insert(id){\n switch (id) {\n' + \
-                                 BuildBlockScreen + '}\n return str;\n}'
-
-                with open(Options.root_dir + "/template/functions.js", 'r') as fhandle:
-                    Functions = '<script type = "text/javascript">\n' + \
-                                fhandle.read() + ShowBBInScreen + \
-                                '</script>'
-
-                with open(Options.root_dir + "/template/tail.html", 'r') as fhandle:
-                    Tail = fhandle.read() + '</body>\n</html>'
-
-                CompletePage = Header + topbar + ButtonMenu + AlgorithmScreen + LogContainer + Functions + Tail
-                csock.sendall(("""HTTP/1.1 200 OK
-						 Content-Type: text/html
-						 Content-Length: """ + str(len(CompletePage)) + """
-						 \n\n""" + CompletePage + """\r\n""").encode())
-
-            # Else there is no file to handle! We response 404
-            else:
-                Response.reply_404(csock)
-        else:  # a file was specified, so we serve it.
+        elif (filename.endswith(".css")):
+            #filepath = "staticMedia/css/"
             self.serve_file(filepath, filename, csock)
+
+        elif ( filename.endswith(".js")):
+            #filepath = "staticMedia/js/"
+            self.serve_file(filepath, filename, csock)
+
+
+        elif (filename is "" or filename == "index.html") and (os.path.isdir(filepath[1:])):
+            (BuildBlockButton, BuildBlockScreen) = BbManager.start(filepath[1:])
+
+            with open(Options.root_dir + "/template/header.html", 'r') as fhandle:
+                Header = '<!DOCTYPE html>\n<html lang="en">\n' + fhandle.read()
+
+            with open(Options.root_dir + "/template/topbar.html", 'r') as fhandle:
+                topbar = '<body>\n<div class="container-fluid">\n' + fhandle.read()
+
+            ButtonMenu = '<div class="row down-container">\n<div class="col-lg-3">\n<div class="row cluster">' + \
+                        BuildBlockButton + '</div>\n</div>\n'
+
+            with open(Options.root_dir + "/template/algoscreen.html", 'r') as fhandle:
+                AlgorithmScreen = fhandle.read()
+
+            with open(Options.root_dir + "/template/logcontainer.html", 'r') as fhandle:
+                LogContainer = fhandle.read() + '</div>\n'
+
+            ShowBBInScreen = 'function insert(id){\n switch (id) {\n' + \
+                             BuildBlockScreen + '}\n return str;\n}'
+
+            with open(Options.root_dir + "/template/functions.js", 'r') as fhandle:
+                Functions = '<script type = "text/javascript">\n' + \
+                            fhandle.read() + ShowBBInScreen + \
+                            '</script>'
+
+            with open(Options.root_dir + "/template/tail.html", 'r') as fhandle:
+                Tail = fhandle.read() + '</body>\n</html>'
+
+            CompletePage = Header + topbar + ButtonMenu + AlgorithmScreen + LogContainer + Functions + Tail
+            csock.sendall(("""HTTP/1.1 200 OK
+                Content-Type: text/html
+                Content-Length: """ + str(len(CompletePage)) + """
+                \n\n""" + CompletePage + """\r\n""").encode())
+
+
+        else:
+             f = self.serve_index()
+             csock.sendall(("""HTTP/1.1 200 OK
+                Content-Type: text/html
+                Content-Length: """ + str(len(f)) + """
+                \n\n""" + f + """\r\n""").encode())
+            
+            #filename = "index.html"
+            #filepath = "/"
+            #self.serve_file(filepath, filename, csock)
+            
--- protocol/HttpRequest.py
+++ protocol/HttpRequest.py
@@ -1,6 +1,7 @@
 from Options import Options
 from filesystem.Filesystem import Filesystem
 from response.Response import Response
+import os
 
 
 class HttpRequest:
@@ -8,8 +9,6 @@ class HttpRequest:
     def serve_file(self, filepath, filename, csock):
         exists = Filesystem.file_exists(filepath, filename)
         if exists:
-            # remember to call BbManager to gernerate the blocks inside index.html file
-            # before serving
             with open(Options.root_dir + filepath + filename, 'r') as fhandle:
                 Response.reply_200(csock, fhandle.read())
         else:
@@ -22,3 +21,19 @@ class HttpRequest:
                 Response.reply_200(csock, fhandle.read())
         else:
             Response.reply_404(csock)
+
+    def serve_index(self):
+        fd = "assignments"
+        data = os.listdir(fd)
+        data_list = []
+        serve_data= ""
+        for i in data:
+            data_list.append(os.path.join(fd, i))
+
+        for j in data_list:
+            serve_data += "<a class='opgaver' href='/"+str(j)+"/'><div>"+str(j)+"</div></a> "
+
+        fh_top = open("top_index.html", "r").read()
+        fh_btm = open("bottom_index.html", "r").read()
+        fh     = fh_top + serve_data +fh_btm
+        return fh
