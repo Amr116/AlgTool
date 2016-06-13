7a419e2a0d3972db28c6ea60cc0b27d84f4484dc
Amr Elsayed <amr11682@hotmail.com>
2016-06-13 18:30:19 +0200

    fix code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from server.HttpServer import HttpServer
 
 if __name__ == '__main__':
-    server = HttpServer('localhost', 8000)
+    server = HttpServer('localhost', 8080)
     server.start_server()
